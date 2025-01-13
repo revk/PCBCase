fa29191ef2a9962676cb2713a80fdbbeda6f7acb
RevK <github@e.gg>
2025-01-13 08:38:20 +0000

    Transparent

--- autocrop.scm
+++ autocrop.scm
@@ -4,15 +4,9 @@
     (image (car (gimp-file-load RUN-NONINTERACTIVE filename filename)))
     (drawable (car (gimp-image-get-active-layer image)))
     )
-
-  ; crop the image
   (plug-in-autocrop RUN-NONINTERACTIVE image drawable)
-
-  ; save in original png format
-  (file-png-save RUN-NONINTERACTIVE image drawable filename filename
-       0 6 0 0 0 1 1)
-
-  ; clean up the image
+  (gimp-palette-set-background '(255 255 255))
+  (file-png-save RUN-NONINTERACTIVE image drawable filename filename 0 6 1 0 0 1 1)
   (gimp-image-delete image)
   )
 )
